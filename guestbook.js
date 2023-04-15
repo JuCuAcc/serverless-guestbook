b8d1659d3aec4099de19e8f83397fd21cb279431
MD. Jashim Uddin <63178573+JuCuAcc@users.noreply.github.com>
2023-04-15 15:30:55 +0600

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/bdbdc86f-4d5b-407d-8fe9-03d1dc6aa855/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/read-guestbook-entries-sequence.json`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/save-guestbook-entries-sequence`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
